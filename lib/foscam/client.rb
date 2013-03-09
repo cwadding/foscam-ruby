9f41861b207d8aba3b79aaa170f6f5485247e0f2
Chris Waddington <cwadding@gmail.com>
2013-03-08 20:44:43 -0500

    Added a bunch of yard documentation

--- lib/foscam/client.rb
+++ lib/foscam/client.rb
@@ -131,6 +131,15 @@ module Foscam
 			connect(@url, @username, @password)
 		end
 
+		##
+		# Connects to the foscam webcam
+
+		# @param url [String] The address to your camera
+		# @param username [String] username to authorize with the camera
+		# @param password [String] password to authorize with the camera
+		# @example connect to a camera
+		# 	client = Foscam::Client.new
+		# 	client.connect('http://192.168.0.1', 'foobar', 'secret')
 		def connect(url, username = nil, password = nil)
 			@url = url
 			@username = username
@@ -139,11 +148,30 @@ module Foscam
 			@connection.basic_auth(@username, @password) unless @username.nil? && @password.nil?
 		end
 
+		##
+		# Obtains a snapshot of the current image
+		# @return [nil, ::MiniMagick::Image]
+		# @example Save a captured image
+		# 	client = Foscam::Client.new(url: 'http://192.168.0.1', username: 'foobar', password: 'secret')
+		# 	image = client.snapshot
+		# 	unless image.nil?
+		# 		image.write('image_filename.jpg')
+		# 	end
 		def snapshot
 			response = @connection.get('snapshot.cgi')
 			response.success? ? ::MiniMagick::Image.read(response.body) : nil
 		end
 
+		##
+		# Obtains the cameras status information
+		# @see DDNS_STATUS
+		# @see UPNP_STATUS
+		# @see ALARM_STATUS
+		# @return [Hash] The cameras status
+		# 	* :now [DateTime] The current time on the camera
+		# 	* :alarm_status [String] Returns an Alarm status
+		# 	* :ddns_status [String] Returns an UPNP status
+		# 	* :upnp_status [String] Returns an DDNS status
 		def get_status
 			response = @connection.get('get_status.cgi')
 			response = response.success? ? parse_response(response) : {}
@@ -156,6 +184,18 @@ module Foscam
 			response
 		end
 
+
+		##
+		# Obtains the cameras parameters (orientation, resolution, contrast, brightness)
+		# @see CAMERA_PARAMS_ORIENTATION
+		# @see CAMERA_PARAMS_MODE
+		# @see CAMERA_PARAMS_RESOLUTION
+		# @return [Hash] The cameras parameters
+		# 	* :flip [String] The camera orientation.
+		# 	* :mode [String] The camera mode.
+		# 	* :resolution [String] The camera resolution.
+		# 	* :brightness [Fixnum] The camera brightness.
+		# 	* :contrast [Fixnum] The camera contrast.
 		def get_camera_params
 			response = @connection.get('get_camera_params.cgi')
 			response = response.success? ? parse_response(response) : {}
@@ -168,6 +208,11 @@ module Foscam
 			response
 		end
 
+		##
+		# Controls the pan and tilt of the camera
+		# @see DECODER_CONTROLS
+		# @param action [Symbol] A symbol corresponding to the desired action.
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def decoder_control(action)
 			case action
 				when Symbol || String
@@ -179,6 +224,18 @@ module Foscam
 			response.success?
 		end
 
+		##
+		# Sets the camera sensor parameters
+		# @see CAMERA_CONTROL_MODE
+		# @see CAMERA_CONTROL_ORIENTATION
+		# @see CAMERA_PARAMS_RESOLUTION
+		# @param [Hash] params Parameters to set
+		# @option params [Symbol,String] :resolution Set to one of the keys or values in CAMERA_PARAMS_RESOLUTION
+		# @option params [Fixnum] :brightness Value between 0 and 255
+		# @option params [Fixnum] :contrast Value between 0 and 6
+		# @option params [Symbol, Integer] :mode Value between 0 and 2 or option in CAMERA_CONTROL_MODE
+		# @option params [Symbol, Integer] :flip Value between 0 and 3 or option in CAMERA_CONTROL_ORIENTATION
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def camera_control(params)
 			params.all? do |key, value|
 				# validation
@@ -230,16 +287,173 @@ module Foscam
 			end
 		end
 
+		##
+		# Reboots the camera
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def reboot
 			response = @connection.get("reboot.cgi")
 			response.success?
 		end
 
+		##
+		# Restore settings to the factory defaults
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def restore_factory
 			response = @connection.get("restore_factory.cgi")
 			response.success?
 		end
 
+		##
+		# Returns all the parameters for the camera
+		# @return [Hash] If the request is unsuccessful the hash will be empty. Otherwise it contains the following fields:
+		# 	* [String] :id The device id.
+		# 	* [String] :sys_ver Firmware version number.
+		# 	* [String] :resolution Web UI version number.
+		# 	* [String] :alias The assigned camera name.
+		# 	* [DateTime] :now The camera's time.
+		# 	* [String] :tz The camera time zone.
+		# 	* [FalseClass, TrueClass] :ntp_enable Whether the ntp server is enabled.
+		# 	* [String] :ntp_svr Address to the ntp server.
+		# 	* [String] :user1_name Username of user1.
+		# 	* [String] :user1_pwd Password of user1.
+		# 	* [String] :user1_pri Privilages of user1.
+		# 	* ...
+		# 	* [String] :user8_name Username of user8.
+		# 	* [String] :user8_pwd Password of user8.
+		# 	* [String] :user8_pri Privilages of user8.
+		# 	* [String] :dev2_alias] The 2nd Device alias 
+		# 	* [String] :dev2_host The 2nd Device host(IP or Domain name) 
+		# 	* [String] :dev2_port The 2nd Device port.
+		# 	* [String] :dev2_user The 2nd Device user name .
+		# 	* [String] :dev2_pwd The 2nd Device password.
+		# 	* ...
+		# 	* [String] :dev9_alias The 9th Device alias 
+		# 	* [String] :dev9_host The 9th Device host(IP or Domain name) 
+		# 	* [Fixnum] :dev9_port The 9th Device port.
+		# 	* [String] :dev9_user The 9th Device user name .
+		# 	* [String] :dev9_pwd The 9th Device password.
+		# 	* [String] :ip_address The network ip address of the camera.
+		# 	* [String] :mask The network mask of the camera.
+		# 	* [String] :gateway The network gateway of the camera.
+		# 	* [String] :dns The address of the dns server.
+		# 	* [Fixnum] :port The network port.
+		# 	* [FalseClass, TrueClass] :wifi_enable Whether wifi is enabled or not.
+		# 	* [String] :wifi_ssid Your WIFI SSID 
+		# 	* [FalseClass, TrueClass] :wifi_encrypt Whether wifi is encrypted or not.
+		# 	* [String] :wifi_defkey Wep Default TX Key 
+		# 	* [String] :wifi_key1 Key1 
+		# 	* [String] :wifi_key2 Key2 
+		# 	* [String] :wifi_key3 Key3 
+		# 	* [String] :wifi_key4 Key4 
+		# 	* [String] :wifi_authtype The Authetication type 0:open 1:share
+		# 	* [String] :wifi_keyformat Keyformat 0:Hex 1:ASCII 
+		# 	* [String] :wifi_key1_bits 0:64 bits; 1:128 bits 
+		# 	* [String] :wifi_key2_bits 0:64 bits; 1:128 bits 
+		# 	* [String] :wifi_key3_bits 0:64 bits; 1:128 bits 
+		# 	* [String] :wifi_key4_bits 0:64 bits; 1:128 bits 
+		# 	* [String] :wifi_channel Channel (default 6) 
+		# 	* [String] :wifi_mode Mode (default 0) 
+		# 	* [String] :wifi_wpa_psk wpa_psk
+		# 	* [FalseClass, TrueClass] :pppoe_enable 
+        # 	* [String] :pppoe_user
+        # 	* [String] :pppoe_pwd
+        # 	* [FalseClass, TrueClass] :upnp_enable
+        # 	* [String] :ddns_service
+        # 	* [String] :ddns_user
+        # 	* [String] :ddns_pwd
+        # 	* [String] :ddns_host
+        # 	* [String] :ddns_proxy_svr
+        # 	* [Fixnum] :ddns_proxy_port
+        # 	* [String] :mail_svr
+        # 	* [Fixnum] :mail_port 
+        # 	* [String] :mail_tls
+        # 	* [String] :mail_user
+        # 	* [String] :mail_pwd
+        # 	* [String] :mail_sender
+        # 	* [String] :mail_receiver1
+        # 	* [String] :mail_receiver2
+        # 	* [String] :mail_receiver3
+        # 	* [String] :mail_receiver4
+        # 	* [String] :mail_inet_ip
+        # 	* [String] :ftp_svr 
+        # 	* [String] :ftp_port 
+        # 	* [String] :ftp_user 
+        # 	* [String] :ftp_pwd 
+        # 	* [String] :ftp_dir 
+        # 	* [String] :ftp_mode 
+        # 	* [String] :ftp_upload_interval 
+        # 	* [String] :ftp_filename 
+        # 	* [Fixnum] :ftp_numberoffiles
+        # 	* [FalseClass, TrueClass] :ftp_schedule_enable 
+        # 	* [Fixnum] :ftp_schedule_sun_0 
+        # 	* [Fixnum] :ftp_schedule_sun_1 
+        # 	* [Fixnum] :ftp_schedule_sun_2 
+        # 	* [Fixnum] :ftp_schedule_mon_0 
+        # 	* [Fixnum] :ftp_schedule_mon_1 
+        # 	* [Fixnum] :ftp_schedule_mon_2 
+        # 	* [Fixnum] :ftp_schedule_tue_0 
+        # 	* [Fixnum] :ftp_schedule_tue_1 
+        # 	* [Fixnum] :ftp_schedule_tue_2 
+        # 	* [Fixnum] :ftp_schedule_wed_0 
+        # 	* [Fixnum] :ftp_schedule_wed_1 
+        # 	* [Fixnum] :ftp_schedule_wed_2 
+        # 	* [Fixnum] :ftp_schedule_thu_0 
+        # 	* [Fixnum] :ftp_schedule_thu_1 
+        # 	* [Fixnum] :ftp_schedule_thu_2 
+        # 	* [Fixnum] :ftp_schedule_fri_0 
+        # 	* [Fixnum] :ftp_schedule_fri_1 
+        # 	* [Fixnum] :ftp_schedule_fri_2 
+        # 	* [Fixnum] :ftp_schedule_sat_0 
+        # 	* [Fixnum] :ftp_schedule_sat_1 
+        # 	* [Fixnum] :ftp_schedule_sat_2 
+        # 	* [FalseClass, TrueClass] :alarm_motion_armed
+        # 	* [Fixnum] :alarm_motion_sensitivity
+        # 	* [Fixnum] :alarm_motion_compensation 
+        # 	* [FalseClass, TrueClass] :alarm_input_armed 
+        # 	* [Fixnum] :alarm_ioin_level 
+        # 	* [Fixnum] :alarm_iolinkage 
+        # 	* [Fixnum] :alarm_preset 
+        # 	* [Fixnum] :alarm_ioout_level 
+        # 	* [FalseClass, TrueClass] :alarm_mail 
+        # 	* [Fixnum] :alarm_upload_interval 
+        # 	* [FalseClass, TrueClass] :alarm_http
+        # 	* [FalseClass, TrueClass] :alarm_msn
+        # 	* [String] :alarm_http_url
+        # 	* [FalseClass, TrueClass] :alarm_schedule_enable
+        # 	* [Fixnum] :alarm_schedule_sun_0 
+        # 	* [Fixnum] :alarm_schedule_sun_1 
+        # 	* [Fixnum] :alarm_schedule_sun_2 
+        # 	* [Fixnum] :alarm_schedule_mon_0 
+        # 	* [Fixnum] :alarm_schedule_mon_1 
+        # 	* [Fixnum] :alarm_schedule_mon_2 
+        # 	* [Fixnum] :alarm_schedule_tue_0 
+        # 	* [Fixnum] :alarm_schedule_tue_1 
+        # 	* [Fixnum] :alarm_schedule_tue_2 
+        # 	* [Fixnum] :alarm_schedule_wed_0 
+        # 	* [Fixnum] :alarm_schedule_wed_1 
+        # 	* [Fixnum] :alarm_schedule_wed_2 
+        # 	* [Fixnum] :alarm_schedule_thu_0 
+        # 	* [Fixnum] :alarm_schedule_thu_1 
+        # 	* [Fixnum] :alarm_schedule_thu_2 
+        # 	* [Fixnum] :alarm_schedule_fri_0 
+        # 	* [Fixnum] :alarm_schedule_fri_1 
+        # 	* [Fixnum] :alarm_schedule_fri_2 
+        # 	* [Fixnum] :alarm_schedule_sat_0 
+        # 	* [Fixnum] :alarm_schedule_sat_1 
+        # 	* [Fixnum] :alarm_schedule_sat_2 
+        # 	* [Fixnum] :decoder_baud 
+        # 	* [String] :msn_user
+        # 	* [String] :msn_pwd
+        # 	* [String] :msn_friend1
+        # 	* [String] :msn_friend2
+        # 	* [String] :msn_friend3
+        # 	* [String] :msn_friend4
+        # 	* [String] :msn_friend5
+        # 	* [String] :msn_friend6
+        # 	* [String] :msn_friend7
+        # 	* [String] :msn_friend8
+        # 	* [String] :msn_friend9
+        # 	* [String] :msn_friend10
 		def get_params
 			response = @connection.get("get_params.cgi")
 			response = response.success? ? parse_response(response) : {}
@@ -248,21 +462,22 @@ module Foscam
 				[:ntp_enable, :wifi_enable, :pppoe_enable, :upnp_enable, :alarm_schedule_enable, :ftp_schedule_enable].each do |field|
 					response[field] = response[field].to_i > 0
 				end
-				[:ftp_schedule_sun_0, :ftp_schedule_sun_1, :ftp_schedule_sun_2, 
-				:ftp_schedule_mon_0, :ftp_schedule_mon_1, :ftp_schedule_mon_2, 
-				:ftp_schedule_tue_0, :ftp_schedule_tue_1, :ftp_schedule_tue_2, 
-				:ftp_schedule_wed_0, :ftp_schedule_wed_1, :ftp_schedule_wed_2, 
-				:ftp_schedule_thu_0, :ftp_schedule_thu_1, :ftp_schedule_thu_2, 
-				:ftp_schedule_fri_0, :ftp_schedule_fri_1, :ftp_schedule_fri_2, 
-				:ftp_schedule_sat_0, :ftp_schedule_sat_1, :ftp_schedule_sat_2,
-				:alarm_schedule_sun_0, :alarm_schedule_sun_1, :alarm_schedule_sun_2, 
-				:alarm_schedule_mon_0, :alarm_schedule_mon_1, :alarm_schedule_mon_2, 
-				:alarm_schedule_tue_0, :alarm_schedule_tue_1, :alarm_schedule_tue_2, 
-				:alarm_schedule_wed_0, :alarm_schedule_wed_1, :alarm_schedule_wed_2, 
-				:alarm_schedule_thu_0, :alarm_schedule_thu_1, :alarm_schedule_thu_2, 
-				:alarm_schedule_fri_0, :alarm_schedule_fri_1, :alarm_schedule_fri_2, 
-				:alarm_schedule_sat_0, :alarm_schedule_sat_1, :alarm_schedule_sat_2,
-				:daylight_savings_time, :ddns_proxy_port, :ftp_port, :mail_port, :port, :dev2_port, :dev3_port, :dev4_port, :dev5_port, :dev6_port, :dev7_port, :dev8_port, :dev9_port].each do |field|
+				[
+					:ftp_schedule_sun_0, :ftp_schedule_sun_1, :ftp_schedule_sun_2, 
+					:ftp_schedule_mon_0, :ftp_schedule_mon_1, :ftp_schedule_mon_2, 
+					:ftp_schedule_tue_0, :ftp_schedule_tue_1, :ftp_schedule_tue_2, 
+					:ftp_schedule_wed_0, :ftp_schedule_wed_1, :ftp_schedule_wed_2, 
+					:ftp_schedule_thu_0, :ftp_schedule_thu_1, :ftp_schedule_thu_2, 
+					:ftp_schedule_fri_0, :ftp_schedule_fri_1, :ftp_schedule_fri_2, 
+					:ftp_schedule_sat_0, :ftp_schedule_sat_1, :ftp_schedule_sat_2,
+					:alarm_schedule_sun_0, :alarm_schedule_sun_1, :alarm_schedule_sun_2, 
+					:alarm_schedule_mon_0, :alarm_schedule_mon_1, :alarm_schedule_mon_2, 
+					:alarm_schedule_tue_0, :alarm_schedule_tue_1, :alarm_schedule_tue_2, 
+					:alarm_schedule_wed_0, :alarm_schedule_wed_1, :alarm_schedule_wed_2, 
+					:alarm_schedule_thu_0, :alarm_schedule_thu_1, :alarm_schedule_thu_2, 
+					:alarm_schedule_fri_0, :alarm_schedule_fri_1, :alarm_schedule_fri_2, 
+					:alarm_schedule_sat_0, :alarm_schedule_sat_1, :alarm_schedule_sat_2,
+					:daylight_savings_time, :ddns_proxy_port, :ftp_port, :mail_port, :port, :dev2_port, :dev3_port, :dev4_port, :dev5_port, :dev6_port, :dev7_port, :dev8_port, :dev9_port].each do |field|
 					response[field] = response[field].to_i
 				end
 				[:user1_pri, :user2_pri, :user3_pri, :user4_pri, :user5_pri, :user6_pri, :user7_pri, :user8_pri].each do |key|
@@ -272,20 +487,35 @@ module Foscam
 			response
 		end
 
+		##
+		# Upgrade the camera firmware
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def upgrade_firmware
 			response = @connection.post("upgrade_firmware.cgi")
 		end
 
+		##
+		# Upgrade the cameras html pages.
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def upgrade_htmls
 			response = @connection.post("upgrade_htmls.cgi")
 		end
 
+
+		##
+		# Set the name of the camera
+		# @param [String] name The name of the camera which is 20 characters or less.
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def set_alias(name)
 			throw "invalid parameter value" if name.length > 20
 			response = @connection.get("set_alias.cgi?alias=#{name}")
 			response.success?
 		end
 
+		##
+		# Set the datetime of the camera
+		# @param [Hash] params
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def set_datetime(params)
 			# Extract the time zone
 			throw "invalid parameter value" if params.has_key?(:ntp_svr) && params[:ntp_svr].length > 64
@@ -293,6 +523,10 @@ module Foscam
 			response.success?
 		end
 
+		##
+		# Set usernames, passwords and privilages
+		# @param [Hash] params
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def set_users(params)
 			[:user1, :pwd1, :user2, :pwd2, :user3, :pwd3, :user4, :pwd4, :user5, :pwd5, :user6, :pwd6, :user7, :pwd7, :user8, :pwd8].each do |key|
 				throw "invalid parameter value" if params.has_key?(key) && params[key].length > 12
@@ -305,16 +539,34 @@ module Foscam
 		# 	response = @connection.get("set_devices.cgi?#{params.to_query}")
 		# end
 
+		##
+		# Set usernames, passwords and privilages
+		# @param [Hash] params
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def set_network(params)
 			response = @connection.get("set_network.cgi?#{params.to_query}")
 			response.success?
 		end
-
+		##
+		# Set the wifi parameters
+		# @param [Hash] params
+		# @option params [FalseClass, TrueClass] :enable Whether wifi is enabled or not.
+		# @option params [String] :ssid Your WIFI SSID 
+		# @option params [FalseClass, TrueClass] :encrypt Whether wifi is encrypted or not.
+		# @option params [String] :wpa_psk wpa_psk
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def set_wifi(params)
 			response = @connection.get("set_wifi.cgi?#{params.to_query}")
 			response.success?
 		end
-
+		
+		##
+		# Set the pppoe parameters
+		# @param [Hash] params
+		# @option params [FalseClass, TrueClass] :enable 
+        # @option params [String] :user
+        # @option params [String] :pwd
+		# @return [FalseClass,TrueClass] whether the request was successful.
 		def set_pppoe(params)
 			throw "invalid parameter value" if params.has_key?(:user) && params[:user].length > 20
 			throw "invalid parameter value" if params.has_key?(:pwd) && params[:pwd].length > 20
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,9 +4,10 @@ require 'bundler/setup'
 require 'vcr'
 require 'foscam-ruby' # and any other gems you need
 
-FOSCAM_USERNAME = 'my_username'
-FOSCAM_PASSWORD = 'my_password'
-FOSCAM_URL = "http://192.168.0.117/"
+
+FOSCAM_USERNAME = 'my_username' #:nodoc:all
+FOSCAM_PASSWORD = 'my_password' #:nodoc:all
+FOSCAM_URL = "http://192.168.0.117/" #:nodoc:all
 
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/fixtures/vcr'
@@ -14,9 +15,15 @@ VCR.configure do |c|
   # c.allow_http_connections_when_no_cassette = true
 end
 
-module Boolean; end
-class TrueClass; include Boolean; end
-class FalseClass; include Boolean; end
+
+module Boolean #:nodoc:all
+end
+class TrueClass #:nodoc:all
+	include Boolean
+end
+class FalseClass #:nodoc:all
+	include Boolean
+end
 
 RSpec.configure do |config|
   # some (optional) config here
